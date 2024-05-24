d927ed4d5671d13c7efaa0f668ed208097df7162
@-Painfuego <kystax69@gmail.com>
2024-05-24 19:12:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+/** @format */
+
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 443;
+
+app.get("/", (req, res) => {
+  res.send('<meta http-equiv="refresh" content="0; URL=/home"/>');
+});
+
+app.get("/home", (req, res) => {
+  res.send(
+    `<meta http-equiv="refresh" content="0; URL=https://discord.gg/1st-952570101784281139"/>`
+  );
+});
+
+app.get("/painfuego", (req, res) => {
+  res.send(
+    `<meta http-equiv="refresh" content="0; URL=https://github.com/painfueg0"/>`
+  );
+});
+
+app.get("/flame", (req, res) => {
+  res.send(
+    `<meta http-equiv="refresh" content="0; URL=https://github.com/flame3301"/>`
+  );
+});
+
+app.get("/fuego", (req, res) => {
+  res.send(
+    `<meta http-equiv="refresh" content="0; URL=https://discord.com/application-directory/1050423676689985606"/>`
+  );
+});
+
+app.get("/flare", (req, res) => {
+  res.send(
+    `<meta http-equiv="refresh" content="0; URL=https://discord.com/application-directory/1032300215664914523"/>`
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Loaded Web server | Port : (${port})`, "success");
+});
